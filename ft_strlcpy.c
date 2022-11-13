165f1041046388a76ac49ea32dd5ccacc2d74a08
Irene Diaz fossaty <idiaz-fo@c1r2s1.42madrid.com>
2022-11-13 17:22:14 +0100

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: idiaz-fo <idiaz-fo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 17:54:51 by idiaz-fo          #+#    #+#             */
-/*   Updated: 2022/11/13 16:29:22 by idiaz-fo         ###   ########.fr       */
+/*   Updated: 2022/11/13 17:18:19 by idiaz-fo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	srcsize = 0;
 	while (src[srcsize] != 0)
 		srcsize++;
-	if (dstsize != 0)
+	if (!dstsize)
+		return (srcsize);
+	while (src[i] != '\0' && i < (dstsize - 1))
 	{
-		while (i < dstsize)
-		{
-			if (i == dstsize - 1)
-				dst[i] = '\0';
-			else
-				dst[i] = src[i];
-			i++;
-		}
+		dst[i] = src[i];
+		i++;
 	}
+	dst[i] = '\0';
 	return (srcsize);
 }
