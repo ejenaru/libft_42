3c98c07caede4590fb4c5c5e343d5f1153515fdb
Irene Diaz fossaty <idiaz-fo@c1r6s3.42madrid.com>
2022-11-13 01:17:07 +0100

    strchr done

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: idiaz-fo <idiaz-fo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 20:48:51 by idiaz-fo          #+#    #+#             */
-/*   Updated: 2022/09/20 18:47:43 by idiaz-fo         ###   ########.fr       */
+/*   Updated: 2022/11/13 01:16:22 by idiaz-fo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strchr(const char *s, int c)
 	size_t	i;
 
 	i = 0;
-	if (!s)
-		return (NULL);
 	while (s[i])
 	{
 		if (s[i] == c)
 			return ((char *)&s[i]);
 		i++;
 	}
+	if (c == '\0' && s[i] == c && i != 0)
+		return ((char *)&s[i]);
 	return (NULL);
 }
